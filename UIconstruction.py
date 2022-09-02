a68741113211127083530236b227f0b959d2e6a4
Blad3sy <oscarprosser@gmail.com>
2022-09-02 17:44:18 +0100

    Edits
    Edits, however currently not working

--- UIconstruction.py
+++ UIconstruction.py
@@ -1,3 +1,4 @@
+from posixpath import dirname
 import tkinter
 
 class Window:
@@ -26,7 +27,36 @@ class Window:
         return self.title
 
     def startWindow(self):
-        Window.tk = tkinter.Tk()
-        Window.tk.geometry(f"{self.width}x{self.height}")
-        Window.tk.title = self.title
-        Window.tk.mainloop()
+        root = tkinter.Tk()
+        root.geometry(f"{self.width}x{self.height}")
+        root.title = self.title
+        root.mainloop()
+
+class Button:
+
+    def __init__(self):
+        self.height = None
+        self.width = None
+        self.label = None
+    
+    def setDimensions(self, height, width):
+        self.height = height
+        self.width = width
+
+    def GetDimensions(self):
+        dimensionsDict = {
+            "Height": self.height,
+            "Width": self.width
+        }
+
+        return dimensionsDict
+    
+    def setLabel(self, labelText):
+        self.label = labelText
+    
+    def getLabel(self):
+        return self.label
+    
+    def createButton(self):
+        button = Button(text = self.label, height = self.height, width = self.width)
+        button.mainloop()
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from UIconstruction import Window
+from UIconstruction import Window, Button
 
 background = Window()
+button1 = Button()
 
 background.setDimensions(500, 500)
 background.startWindow()
+
+button1.setDimensions(100, 100)
+button1.setLabel("test")
+button1.createButton()
