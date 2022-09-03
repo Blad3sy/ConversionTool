ae81eb145ab46edb0c40cfd47e92ab6015f9f303
Blad3sy <oscarprosser@gmail.com>
2022-09-03 12:31:47 +0100

    Button now working, working on Label

--- UIconstruction.py
+++ UIconstruction.py
@@ -25,37 +25,98 @@ class Window:
     def getTitle(self):
         return self.title
 
+    def createWindow(self):
+        self.tk = tkinter.Tk()
+        self.tk.geometry(f"{self.width}x{self.height}")
+        self.tk.title(self.title)
+    
     def startWindow(self):
-        Window.tk = tkinter.Tk()
-        Window.tk.geometry(f"{self.width}x{self.height}")
-        Window.tk.title(self.title)
-        Window.tk.mainloop()
+        self.tk.mainloop()
 
 class Button:
 
-    def __init__(self):
+    def __init__(self, parent):
         self.height = None
         self.width = None
         self.label = None
-        self.container = None
+        self.xloc = None
+        self.yloc = None
+        self.parent = parent
     
     def setDimensions(self, height, width):
         self.height = height
         self.width = width
 
-    def GetDimensions(self):
+    def getDimensions(self):
         dimensionsDict = {
             "Height": self.height,
             "Width": self.width
         }
 
         return dimensionsDict
+
+    def setLocation(self, x, y):
+        self.xloc = x
+        self.yloc = y
+
+    def getLocation(self):
+        locationDict = {
+            "X": self.xloc,
+            "Y": self.yloc
+        }
+
+        return locationDict
     
     def setLabel(self, labelText):
         self.label = labelText
     
     def getLabel(self):
-        return self.label   
+        return self.label      
    
     def createButton(self):
-        button = tkinter.Button(text = self.label, width = self.width, height = self.height)
+        self.button = tkinter.Button(self.parent, text=self.label, width=self.width, height=self.height)
+        self.button.place(x=self.xloc, y=self.yloc)
+
+class Label:
+
+    def __init__(self, parent):
+        self.height = None
+        self.width = None
+        self.text = None
+        self.xloc = None
+        self.yloc = None
+        self.parent = parent
+    
+    def setDimensions(self, height, width):
+        self.height = height
+        self.width = width
+
+    def getDimensions(self):
+        dimensionsDict = {
+            "Height": self.height,
+            "Width": self.width
+        }
+
+        return dimensionsDict
+
+    def setLocation(self, x, y):
+        self.xloc = x
+        self.yloc = y
+
+    def getLocation(self):
+        locationDict = {
+            "X": self.xloc,
+            "Y": self.yloc
+        }
+
+        return locationDict
+    
+    def setText(self, inputText):
+        self.text = inputText
+    
+    def getLabel(self):
+        return self.text
+
+    def createLabel(self):
+        self.label = tkinter.Label(self.parent, text=self.label, width=self.width, height=self.height)
+        self.label.place(x=self.xloc, y=self.yloc) 
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from UIconstruction import Window, Button
 
 background = Window()
-button1 = Button()
 
 background.setDimensions(500, 500)
 background.setTitle("Test")
-background.startWindow()
+background.createWindow()
 
-button1.setDimensions(100, 100)
+button1 = Button(background.tk)
+
+button1.setDimensions(5, 10)
 button1.setLabel("test")
+button1.setLocation(150, 150)
 button1.createButton()
+
+background.startWindow()
